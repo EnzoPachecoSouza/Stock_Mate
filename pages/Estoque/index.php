8245a3ebec00ff2e37700f1a7af0b6376002e49e
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-10 21:17:30 -0300

    Alterando a estrutura de arquivos para facilitar a estilização e uma possível componentização

--- pages/Estoque/index.php
+++ pages/Estoque/index.php
@@ -10,8 +10,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
     <!-- CSS -->
-    <link rel="stylesheet" href="../../styles/styles.css">
+    <link rel="stylesheet" href="./styles.css">
 </head>
 
 <body>
@@ -143,13 +146,7 @@
     <!----------------------->
 
     <!-- CADASTRAR PRODUTO -->
-    <!-- <div class="modal">
-        <div class="modal-box">
-            <form class="container" action="">
-
-            </form>
-        </div>
-    </div> -->
+    
     <!----------------------->
 
     <!-- CADASTRAR PRODUTO -->
