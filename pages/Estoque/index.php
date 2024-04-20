925f7d86129b1a14912bca3c693523ee4b8ca598
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-20 15:33:07 -0300

    Inserindo modal do bootstrap

--- pages/Estoque/index.php
+++ pages/Estoque/index.php
@@ -66,8 +66,7 @@
             <button class="btn btn-lg btn-primary">Saída</button>
 
             <!-- BOTÃO DE CRIAR PRODUTO -->
-            <button type="button" class="btn btn-primary rounded-circle" style="position: absolute; top: 0; right: 0"
-                data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <i class="bi bi-plus-lg"></i>
             </button>
             <!------->
@@ -130,6 +129,23 @@
     </div>
 
     <!-- CADASTRAR PRODUTO -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- <div class="modal">
         <div class="modal-box">
             <form class="container" action="">
