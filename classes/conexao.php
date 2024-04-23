ce8da4bc2c3d52764829b06b178499af0a66163b
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-23 17:55:54 -0300

    Criando função de conexão

--- classes/conexao.php
+++ classes/conexao.php
@@ -7,5 +7,15 @@ class Conexao
     private $user = 'root';
     private $pass = '';
 
-    
+    public function conectar()
+    {
+        try {
+            $conexao = new PDO("mysql:host=$this->host;dbname=$this->dbname","$this->user","$this->pass");
+
+            return $conexao;
+            
+        } catch (PDOException $e) {
+            
+        }
+    }
 }
