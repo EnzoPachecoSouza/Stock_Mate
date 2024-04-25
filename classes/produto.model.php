d8d1f3a58a8fbbd142247f270b4394eaa73fab24
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-25 14:48:37 -0300

    Criando classe de produto (produto.model) e definindo seus atributos

---
+++ classes/produto.model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Produto
+{
+    private $codigo;
+    private $nome;
+    private $cor;
+    private $material;
+    private $categoria;
+    private $detalhes;
+    private $precoDeCompra;
+    private $quantidadeEmEstoque;
+    private $estoqueMinimo;
+    private $descricao;
+
+    
+}
