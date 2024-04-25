a13b86667d06563f8512f7e059326b6580c61ce0
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-25 14:58:23 -0300

    Criando classe construtora do produto (produto.service) para fazer a conexão com o BD e receber os atributos do produto ao ser instanciado

--- classes/produto.service.php
+++ classes/produto.service.php
@@ -4,4 +4,10 @@ class ProdutoService
 {
     private $conexao;
     private $produto;
+
+    private function __construct(Conexao $conexao, Produto $produto)
+    {
+        $this->conexao = $conexao->conectar();
+        $this->produto = $produto;
+    }
 }
