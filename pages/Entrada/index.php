d3c978b00354fcb904eb49751ce04a1a88bf339e
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-05-09 19:25:26 -0300

    Tirando botões do modal de registrar fornecedor

--- pages/Entrada/index.php
+++ pages/Entrada/index.php
@@ -196,8 +196,6 @@ require '../../classes/Fornecedor/fornecedor.controller.php';
                                 <div class="modal-content">
                                     <div class="modal-header">
                                         <h6 class="modal-title">Registrar Fornecedor</h6>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
                                     </div>
 
                                     <div class="modal-body">
@@ -220,7 +218,7 @@ require '../../classes/Fornecedor/fornecedor.controller.php';
                                                 </div>
                                             </div>
 
-                                            <div class="row mb-5">
+                                            <div class="row">
                                                 <div class="col-md-6">
                                                     <div class="form-floating">
                                                         <input class="form-control" type="text" id="" name=""
@@ -236,10 +234,6 @@ require '../../classes/Fornecedor/fornecedor.controller.php';
                                                         <label for="">CNPJ</label>
                                                     </div>
                                                 </div>
-                                            </div>
-
-                                            <div class="d-flex justify-content-center align-items-center">
-                                                <button class="btn btn-outline-primary">Registrar Fornecedor</button>
                                             </div>
                                         </form>
                                     </div>
@@ -294,7 +288,7 @@ require '../../classes/Fornecedor/fornecedor.controller.php';
                             </div>
                         </div>
 
-                        <div class="row">
+                        <div class="row" id="produtosEntrada">
                             <div class="col-md-4">
                                 <div class="form-floating">
                                     <select class="form-select" id="" name="">
