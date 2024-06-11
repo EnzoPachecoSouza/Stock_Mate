9b55aedf49d1bf2a71ec9dd1fccd4a71aeff643b
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-06-11 16:15:07 -0300

    Criando arquivo de classes de itens da entrada

---
+++ classes/Entrada/IntensEntrada/itensEntrada.controller.php
@@ -0,0 +1,47 @@
+<?php
+require "entrada.model.php";
+require "entrada.service.php";
+require "../../classes/conexao.php";
+
+$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+
+if ($acao == 'inserir') {
+    $entrada = new Entrada();
+
+    $entrada->__set('dataCompra', $_POST['dataCompra']);
+    $entrada->__set('valorTotal', $_POST['valorTotal']);
+    $entrada->__set('dataPagamento', $_POST['dataPagamento']);
+    $entrada->__set('fornecedor', $_POST['fornecedor']);
+    $entrada->__set('formaPagamento', $_POST['formaPagamento']);
+
+    $conexao = new Conexao();
+
+    $entradaService = new EntradaService($conexao, $entrada);
+    $entradaService->inserir();
+
+    header('Location: ../../pages/Entrada/index.php?act=inserir');
+} else if ($acao == 'recuperar') {
+    $entrada = new Entrada();
+
+    $conexao = new Conexao();
+
+    $entradaService = new EntradaService($conexao, $entrada);
+    $entradas = $entradaService->recuperar();
+} else if ($acao == 'editar') {
+    $id = isset($_GET['id']) ? $_GET['id'] : $id;
+
+    $entrada = new Entrada();
+
+    $entrada->__set('dataCompra', $_POST['dataCompra']);
+    $entrada->__set('valorTotal', $_POST['valorTotal']);
+    $entrada->__set('dataPagamento', $_POST['dataPagamento']);
+    $entrada->__set('formaPagamento', $_POST['formaPagamento']);
+    $entrada->__set('fornecedor', $_POST['fornecedor']);
+
+    $conexao = new Conexao();
+
+    $entradaService = new EntradaService($conexao, $entrada);
+    $entradaService->editar($id);
+
+    header('Location: ../../pages/Entrada/index.php?act=editar');
+}
---
+++ classes/Entrada/IntensEntrada/itensEntrada.model.php
@@ -0,0 +1,21 @@
+<?php
+
+class Entrada
+{
+    private $dataCompra;
+    private $valorTotal;
+    private $dataPagamento;
+    private $formaPagamento;
+    private $status;
+    private $fornecedor;
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
+
+    public function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+}
