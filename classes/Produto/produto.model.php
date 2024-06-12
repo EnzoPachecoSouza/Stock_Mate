021ad1efb7463df66116146bccf3602a10d7440f
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-06-12 17:05:55 -0300

    Corrigindo problema de instanciamento de classe em produtos

--- classes/Produto/produto.model.php
+++ classes/Produto/produto.model.php
@@ -1,27 +1,30 @@
 <?php
 //CLASSE DO PRODUTO
-class Produto
-{
-    private $codigo;
-    private $nome;
-    private $material;
-    private $descricao;
-    private $detalhes;
-    private $cor;
-    private $estoqueMinimo;
-    private $quantidadeEmEstoque;
-    private $precoDeCompra;
-    private $precoDeVenda;
-    private $status;
-    private $categoria;
 
-    public function __set($atributo, $valor)
+if (!class_exists('Conexao')) {
+    class Produto
     {
-        $this->$atributo = $valor;
-    }
+        private $codigo;
+        private $nome;
+        private $material;
+        private $descricao;
+        private $detalhes;
+        private $cor;
+        private $estoqueMinimo;
+        private $quantidadeEmEstoque;
+        private $precoDeCompra;
+        private $precoDeVenda;
+        private $status;
+        private $categoria;
 
-    public function __get($atributo)
-    {
-        return $this->$atributo;
+        public function __set($atributo, $valor)
+        {
+            $this->$atributo = $valor;
+        }
+
+        public function __get($atributo)
+        {
+            return $this->$atributo;
+        }
     }
 }
