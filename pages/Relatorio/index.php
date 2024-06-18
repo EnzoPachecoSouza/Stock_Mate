76e35a07187bf45081c6d8b66c9885e42f5727dc
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-06-18 19:23:24 -0300

    Incluindo função de mostrar toast na pagina de relatórios

--- pages/Relatorio/index.php
+++ pages/Relatorio/index.php
@@ -6,115 +6,115 @@ $cargo_usuario = isset($_SESSION['cargo']) ? $_SESSION['cargo'] : '';
 ?>
 
 <?php if ($_SESSION['cargo'] === 'Gerente') { ?>
-<!DOCTYPE html>
-<html lang="pt-br">
+    <!DOCTYPE html>
+    <html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StockMate | Estoque</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>StockMate | Estoque</title>
 
-    <!-- Font [Noto Sans] -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Anek+Devanagari:wght@100..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
+        <!-- Font [Noto Sans] -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Anek+Devanagari:wght@100..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet">
 
-    <!-- Font [Kanit] -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+        <!-- Font [Kanit] -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <!-- Bootstrap Icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-    <!-- Custom Style -->
-    <link rel="stylesheet" href="../../styles/custom.css">
+        <!-- Custom Style -->
+        <link rel="stylesheet" href="../../styles/custom.css">
 
-    <!-- Style -->
-    <link rel="stylesheet" href="./styles.css">
+        <!-- Style -->
+        <link rel="stylesheet" href="./styles.css">
 
-    <!-- Global CSS -->
-    <link rel="stylesheet" href="../../styles/global.css">
-</head>
+        <!-- Global CSS -->
+        <link rel="stylesheet" href="../../styles/global.css">
+    </head>
 
-<body>
-    <!-- NAVBAR -->
-    <div class="container-fluid bg-primary navbar-shadow">
-        <nav class="container">
-            <div class="row d-flex align-items-center py-3">
+    <body>
+        <!-- NAVBAR -->
+        <div class="container-fluid bg-primary navbar-shadow">
+            <nav class="container">
+                <div class="row d-flex align-items-center py-3">
 
-                <!-- LOGO -->
-                <div class="col-2">
-                    <img src="../../images/logo_stock_mate.png" alt="Logo Stock Mate" class="img-fluid">
-                </div>
-                <!------->
+                    <!-- LOGO -->
+                    <div class="col-2">
+                        <img src="../../images/logo_stock_mate.png" alt="Logo Stock Mate" class="img-fluid">
+                    </div>
+                    <!------->
 
                 <!-- NAVIGATION -->
-                <div class="col-9">
-                    <ul class="nav justify-content-center">
-                        <li class="nav-item">
-                            <a class="nav-link text-white nav-font" href="../estoque">Estoque</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white nav-font" href="../entrada">Entrada</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white nav-font" href="../saida">Saída</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white nav-font" href="../fornecedor">Fornecedores</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white nav-font" href="../cliente">Clientes</a>
-                        </li>
-                        <?php
-                        // Verificar o cargo do usuário para exibir ou ocultar itens do menu
-                        if ($_SESSION['cargo'] === 'Gerente') { ?>
+                    <div class="col-9">
+                        <ul class="nav justify-content-center">
                             <li class="nav-item">
-                                <a class="nav-link text-white nav-font" href="../colaborador">Colaboradores</a>
+                                <a class="nav-link text-white nav-font" href="../estoque">Estoque</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white nav-font selected" href="">Relatórios</a>
+                                <a class="nav-link text-white nav-font" href="../entrada">Entrada</a>
                             </li>
-                        <?php } ?>
-                    </ul>
-                </div>
-
-                <!------->
-
-                <!-- SAIR -->
-                <div class="col-1 text-end">
-                    <div class="dropdown-center">
-                        <button class="btn rounded-circle border-0 p-0" type="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            <i class="bi bi-person-circle fs-1 text-secondary"></i>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <button type="button" class="dropdown-item" data-bs-toggle="modal"
-                                    data-bs-target="#alterarSenhaModal">
-                                    Alterar senha
-                                </button>
+                            <li class="nav-item">
+                                <a class="nav-link text-white nav-font" href="../saida">Saída</a>
                             </li>
-                            <li>
-                                <a class="dropdown-item" href="../logoff.php">
-                                    Sair
-                                    &#8287;&#8287;&#8287;
-                                    <i class="bi bi-box-arrow-right text-primary"></i>
-                                </a>
+                            <li class="nav-item">
+                                <a class="nav-link text-white nav-font" href="../fornecedor">Fornecedores</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white nav-font" href="../cliente">Clientes</a>
+                            </li>
+                            <?php
+                            // Verificar o cargo do usuário para exibir ou ocultar itens do menu
+                            if ($_SESSION['cargo'] === 'Gerente') { ?>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white nav-font" href="../colaborador">Colaboradores</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white nav-font selected" href="">Relatórios</a>
+                                </li>
+                            <?php } ?>
                         </ul>
                     </div>
-                </div>
-                <!------->
+
+                    <!------->
+
+                <!-- SAIR -->
+                    <div class="col-1 text-end">
+                        <div class="dropdown-center">
+                            <button class="btn rounded-circle border-0 p-0" type="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <i class="bi bi-person-circle fs-1 text-secondary"></i>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <button type="button" class="dropdown-item" data-bs-toggle="modal"
+                                        data-bs-target="#alterarSenhaModal">
+                                        Alterar senha
+                                    </button>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="../logoff.php">
+                                        Sair
+                                        &#8287;&#8287;&#8287;
+                                        <i class="bi bi-box-arrow-right text-primary"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!------->
             </div>
         </nav>
     </div>
@@ -158,87 +158,105 @@ $cargo_usuario = isset($_SESSION['cargo']) ? $_SESSION['cargo'] : '';
 
         <div class="mt-3">
             <!--  -->
+            </div>
         </div>
-    </div>
+
+        <!-- TOAST DE CONFIRMAR AÇÃO REALIZADA -->
+        <div class="toast-container position-fixed top-0 end-0 p-3">
+            <div class="toast bg-white" id="toast">
+                <?php $toastAcao = isset($_GET['act']) ? $_GET['act'] : $toastAcao; ?>
+                <?php if ($toastAcao === 'alterarSenha') { ?>
+                    <div class="toast-header fs-5">
+                                <i class="bi bi-square-fill text-info"></i>
+                                <strong class="me-auto ms-3">Alterar Senha</strong>
+                                <button type="button" class="btn-close" onclick="closeToast()"></button>
+                            </div>
+                            <div class="toast-body fs-6">
+                                <strong>Senha alterada com sucesso!</strong>
+                            </div>
+                <?php } ?>
+            </div>
+        </div>
+        <!----------------------->
 
     <!-- ALTERAR SENHA -->
-    <div class="modal fade" id="alterarSenhaModal" tabindex="-1" aria-labelledby="alterarSenhaModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="alterarSenhaModalLabel">Alterar Senha</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form class="container" method="post"
-                        action="../../classes/Colaborador/colaborador.controller.php?acao=alterarSenha&id=<?= $_SESSION['id'] ?>">
-                        <div class="row mb-4">
-                            <div class="col-md-12">
-                                <div class="form-floating">
-                                <input class="form-control" type="text" id="senhaAtual" name="senhaAtual"
-                                    placeholder="Senha Atual" oninput="verificaSenha(this.value)">
-                                <label for="senhaAtual">Senha Atual</label>
+        <div class="modal fade" id="alterarSenhaModal" tabindex="-1" aria-labelledby="alterarSenhaModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="alterarSenhaModalLabel">Alterar Senha</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form class="container" method="post"
+                            action="../../classes/Colaborador/colaborador.controller.php?acao=alterarSenha&id=<?= $_SESSION['id'] ?>">
+                            <div class="row mb-4">
+                                <div class="col-md-12">
+                                    <div class="form-floating">
+                                        <input class="form-control" type="text" id="senhaAtual" name="senhaAtual"
+                                            placeholder="Senha Atual" oninput="verificaSenha(this.value)">
+                                        <label for="senhaAtual">Senha Atual</label>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        
-                        <div class="row mb-2">
-                            <div class="col-md-12">
-                                <div class="form-floating">
-                                    <input class="form-control" type="text" id="novaSenha" name="novaSenha"
-                                        placeholder="Senha Nova" disabled>
-                                    <label for="novaSenha">Senha Nova</label>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="row mb-5">
-                            <div class="col-md-12">
-                                <div class="form-floating">
-                                    <input class="form-control" type="text" id="confirmarSenhaNova" name="confirmarSenhaNova"
-                                        placeholder="Confirme a Senha Nova" disabled>
-                                    <label for="confirmarSenhaNova">Confirme a Senha Nova</label>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="d-flex justify-content-center align-items-center">
-                            <button class="btn btn-outline-primary">Alterar Senha</button>
-                        </div>
-                    </form>
+                            <div class="row mb-2">
+                                <div class="col-md-12">
+                                    <div class="form-floating">
+                                        <input class="form-control" type="text" id="novaSenha" name="novaSenha"
+                                            placeholder="Senha Nova" disabled>
+                                        <label for="novaSenha">Senha Nova</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row mb-5">
+                                <div class="col-md-12">
+                                    <div class="form-floating">
+                                        <input class="form-control" type="text" id="confirmarSenhaNova"
+                                            name="confirmarSenhaNova" placeholder="Confirme a Senha Nova" disabled>
+                                        <label for="confirmarSenhaNova">Confirme a Senha Nova</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="d-flex justify-content-center align-items-center">
+                                <button class="btn btn-outline-primary">Alterar Senha</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!----------------------->
+        <!----------------------->
 
     <script>
-        function verificaSenha(senha){
+        function verificaSenha(senha) {
             const senhaAtual = "<?= $_SESSION['senha'] ?>"
             const senhaNovaInput = document.querySelector('#novaSenha')
             const senhaNovaConfirmaInput = document.querySelector('#confirmarSenhaNova')
 
-            if(senha === senhaAtual){
+            if (senha === senhaAtual) {
                 senhaNovaInput.removeAttribute('disabled')
                 senhaNovaConfirmaInput.removeAttribute('disabled')
             }
         }
     </script>
 
-    <?php include "../../classes/Relatorio/relatorio.php";?>
+    <?php include "../../classes/Relatorio/relatorio.php"; ?>
 
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+            </script>
 
-    <!-- Script -->
-    <script src="../../js/script.js"></script>
-</body>
+        <!-- Script -->
+        <script src="../../js/script.js"></script>
+    </body>
 
-</html>
+    </html>
 
-<?php } else{
+<?php } else { 
     header("Location: ../Login/index.php");
-}?>
+} ?>
