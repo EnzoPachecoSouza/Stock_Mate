85124d4c8b7b5fa32eb8d3c80b1effc957eb76aa
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-06-21 23:08:24 -0300

    Responsividade da navbar da tela de entrada

--- pages/Entrada/index.php
+++ pages/Entrada/index.php
@@ -56,35 +56,40 @@ require '../../classes/Produto/produto.controller.php';
         <nav class="container">
             <div class="row d-flex align-items-center py-3">
 
+                <!-- MENU -->
+                <div class="d-block d-lg-none col-3">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="bi bi-list fs-1 text-white"></i>
+                    </button>
+                </div>
+                <!------->
+
                 <!-- LOGO -->
-                <div class="col-2">
+                <div class="col-6 col-lg-2">
                     <img src="../../images/logo_stock_mate.png" alt="Logo Stock Mate" class="img-fluid">
                 </div>
                 <!------->
 
                 <!-- NAVIGATION -->
-                <div class="col-9">
+                <div class="d-none d-lg-block col-9">
                     <ul class="nav justify-content-center">
                         <li class="nav-item">
                             <a class="nav-link text-white nav-font" href="../estoque">Estoque</a>
                         </li>
-
                         <li class="nav-item">
                             <a class="nav-link text-white nav-font selected" href="../entrada">Entrada</a>
                         </li>
-
                         <li class="nav-item">
                             <a class="nav-link text-white nav-font" href="../saida">Saída</a>
                         </li>
-
                         <li class="nav-item">
                             <a class="nav-link text-white nav-font" href="../fornecedor">Fornecedores</a>
                         </li>
-
                         <li class="nav-item">
                             <a class="nav-link text-white nav-font" href="../cliente">Clientes</a>
                         </li>
-
                         <?php
                         // Verificar o cargo do usuário para exibir ou ocultar itens do menu
                         if ($_SESSION['cargo'] === 'Gerente') { ?>
@@ -100,7 +105,7 @@ require '../../classes/Produto/produto.controller.php';
                 <!------->
 
                 <!-- SAIR -->
-                <div class="col-1 text-end">
+                <div class="col-3 col-lg-1 text-end">
                     <div class="dropdown-center">
                         <button class="btn rounded-circle border-0 p-0" type="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
@@ -126,6 +131,36 @@ require '../../classes/Produto/produto.controller.php';
                 <!------->
             </div>
         </nav>
+
+        <div class="collapse container d-lg-none" id="navbarToggleExternalContent">
+            <ul class="nav d-flex flex-column">
+                <li class="nav-item d-flex mb-1">
+                    <a class="nav-link text-white nav-font" href="../estoque">Estoque</a>
+                </li>
+                <li class="nav-item d-flex mb-1">
+                    <a class="nav-link text-white nav-font selected" href="../entrada">Entrada</a>
+                </li>
+                <li class="nav-item d-flex mb-1">
+                    <a class="nav-link text-white nav-font" href="../saida">Saída</a>
+                </li>
+                <li class="nav-item d-flex mb-1">
+                    <a class="nav-link text-white nav-font" href="../fornecedor">Fornecedores</a>
+                </li>
+                <li class="nav-item d-flex mb-1">
+                    <a class="nav-link text-white nav-font" href="../cliente">Clientes</a>
+                </li>
+                <?php
+                // Verificar o cargo do usuário para exibir ou ocultar itens do menu
+                if ($_SESSION['cargo'] === 'Gerente') { ?>
+                <li class="nav-item d-flex mb-1">
+                    <a class="nav-link text-white nav-font" href="../colaborador">Colaboradores</a>
+                </li>
+                <li class="nav-item d-flex mb-1">
+                    <a class="nav-link text-white nav-font" href="../relatorio">Relatórios</a>
+                </li>
+                <?php } ?>
+            </ul>
+        </div>
     </div>
     <!------->
 
@@ -480,9 +515,9 @@ require '../../classes/Produto/produto.controller.php';
                 <select class="form-select produto" name="produto[]" oninput="determinaValorUnitario(this)" required>
                     <option value="" selected></option>
                     <?php foreach ($produtos as $produto) { ?>
-                            <?php if ($produto->PRO_STATUS === 1) { ?>
-                                <option value="<?= $produto->PRO_PRECO_VENDA ?>-<?= $produto->PRO_ID ?>"><?= $produto->PRO_NOME ?></option>
-                        <?php } ?>
+                                <?php if ($produto->PRO_STATUS === 1) { ?>
+                                        <option value="<?= $produto->PRO_PRECO_VENDA ?>-<?= $produto->PRO_ID ?>"><?= $produto->PRO_NOME ?></option>
+                            <?php } ?>
                     <?php } ?>
                 </select>
                 <label>Produto</label>
